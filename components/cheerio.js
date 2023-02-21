8d050679058e59063d0f6e63dee613e03f6f7727
Alex Cadevall Baulies <alexcadevall@gmail.com>
2023-02-21 15:57:10 +0100

    web scraper

--- components/cheerio.js
+++ components/cheerio.js
@@ -5,12 +5,19 @@ async function dataCheck() {
     try {
         const data = await getInfo()
         const $ = cheerio.load(data)
+        const dataArray = []
 
         $(process.env.WHAT_TO_SCRAPE, data).each(function () {
             const element = $(this).text()
             //in case you also want hrefs
             const links = $(this).fins('a').attr('href')
+
+            dataArray.push({
+                element,
+                links
+            })
         })
+        return dataArray
     } catch (err) {
         console.log(err)
     }
